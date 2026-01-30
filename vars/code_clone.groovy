71185901708c977384bb3b61ce53619affef37c0
REY1405 <kathal.rishi@gmail.com>
2026-01-30 15:24:22 +0530

    added code cloning

--- vars/code_clone.groovy
+++ vars/code_clone.groovy
@@ -1,5 +1,5 @@
-def call(){
+def call(String url, String branch){
     echo "This is cloning the code"
-    git url : "https://github.com/LondheShubham153/django-notes-app.git", branch: "main"
+    git url : url, branch: branch
     echo "code cloning completed"
-}
+} 
